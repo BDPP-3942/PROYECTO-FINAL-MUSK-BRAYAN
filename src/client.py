e50fe63a091b167539f3f5c78e8f15b639d43308
BDPP-3942 <brayanpp012@gmail.com>
2026-07-02 09:54:46 +0200

    Create client.py

---
+++ src/client.py
@@ -0,0 +1,16 @@
+import datetime
+
+class Client:
+    def __init__(self,client_id:int,name:str,country:str,signup_date:datetime):
+        self.client_id = client_id
+        self.name = name
+        self.country = country
+        self.signup_date = signup_date
+    
+    def to_dict(self):
+        return dict({
+            "client_id": self.client_id,
+            "name": self.country,
+            "country": self.country,
+            "signup_date": self.signup_date
+        })
