9d851adc1f250b92e8ae0b6a767a9d758f029ae2
BDPP-3942 <brayanpp012@gmail.com>
2026-07-02 11:53:00 +0200

    Create sale.py

---
+++ src/sale.py
@@ -0,0 +1,21 @@
+import datetime
+
+class Sale:
+    def __init__(self,sale_id:int,client_id:int,product:str,category:str,amount:int,date:datetime):
+        self.sale_id = sale_id
+        self.client_id = client_id
+        self.product = product
+        self.category = category
+        self.amount = amount
+        self.date = date
+
+    def to_dict(self):
+        return {
+            "sale_id": self.sale_id,
+            "client_id": self.client_id,
+            "product": self.product,
+            "category": self.category,
+            "amount": self.amount,
+            "date": self.date
+        }
+    
